ebc0e4da4415d9ef067a475ac761d6b439c14b83
Alexis <alexis.prunier@securitymadein.lu>
2022-09-23 16:34:30 +0200

    Review favicon

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,14 @@ import "react-notifications/lib/notifications.css";
 import { BrowserRouter } from "react-router-dom";
 import { withCookies } from "react-cookie";
 import InsideApp from "./component/InsideApp.jsx";
+import { getApiURL } from "./utils/env.jsx";
 
 class App extends React.Component {
+	// eslint-disable-next-line class-methods-use-this
+	componentDidMount() {
+		document.getElementById("favicon").href = getApiURL() + "public/get_public_image/favicon.ico";
+	}
+
 	render() {
 		return (
 			<div id="App">
